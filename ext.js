fbb6aa4e8962a0c10c53eaba7857743255d8537c
Unity123 <38165550+Unity123@users.noreply.github.com>
2018-05-03 18:19:52 -0600

    Update ext.js

--- ext.js
+++ ext.js
@@ -1,6 +1,7 @@
 (function(ext) {
     // Cleanup function when the extension is unloaded
     ext._shutdown = function() {};
+    var done_with_request = false;
 
     // Status reporting code
     // Use this to report missing hardware, plugin or unsupported browser
@@ -9,12 +10,22 @@
     };
     ext.get_external_data = function(address, type, callback){
       $.ajax({url: address, dataType: type, success: function(data){
+        done_with_request = true;
         callback(data);
       }});
+    }
+    
+    ext.on_external_data = function() {
+      if (done_with_request === true) {
+        done_with_request = false;
+        return true;
+      }
+      return false;
     }
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
+          ['h', 'when external data recieved', 'on_external_data'],
           ['R', 'get external data with address %s and type %s', 'get_external_data', '', '']
         ]
     };
